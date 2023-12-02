724bda14e4c8bb66301ad70c3646e94029b91d21
Konstantin Magin <konstantinmagin@yandex.ru>
2023-12-02 14:44:21 +0300

    Initial Commit

---
+++ robot/src/main/java/com/robot/search/App.java
@@ -0,0 +1,13 @@
+package com.robot.search;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
